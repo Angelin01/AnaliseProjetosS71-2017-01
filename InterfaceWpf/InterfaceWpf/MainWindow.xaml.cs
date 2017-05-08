b5eff3e494e7a3ab605967e493e5b1fd3e23594b
akiragondo1 <gustavogondo@alunos.utfpr.edu.br>
2017-05-07 21:28:15 -0300

    Inserção do projeto em WPF com a janela de registro

---
+++ InterfaceWpf/InterfaceWpf/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InterfaceWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+       
+
+        private void TextBox_EmailA(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Endereco(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Nome(object sender, TextChangedEventArgs e)
+        {
+        
+        }
+
+        private void TextBox_Email(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_rg(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Telefone(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_ctps(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_nomeDoPai(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_NomeDaMae(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Login(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Senha(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Salario(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TelefoneCel(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_Cpf(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void ComboBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ComboBoxItem_Selected_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+
+        private void ComboBoxItem_Funcionario(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ComboBoxItem_Admin(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
