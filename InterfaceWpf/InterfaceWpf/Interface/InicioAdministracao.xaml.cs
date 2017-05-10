12e7126e1c924609486da5458a2209ab84f13182
akiragondo1 <gustavogondo@alunos.utfpr.edu.br>
2017-05-09 21:18:23 -0300

    Segunda tentativa de adição das janelas de consulta

---
+++ InterfaceWpf/InterfaceWpf/Interface/InicioAdministracao.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InterfaceWpf.Interface
+{
+    /// <summary>
+    /// Interaction logic for InicioAdministracao.xaml
+    /// </summary>
+    public partial class InicioAdministracao : Window
+    {
+        public InicioAdministracao()
+        {
+            InitializeComponent();
+        }
+
+        private void RegistrarPedido(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ConsultarPedidos(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RegistrarFuncionario(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ConsultarFuncionarios(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RegistrarProduto(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ConsultarProdutos(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RegistrarIngrediente(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ConsultarIngredientes(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AdicionarTransacao(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ConsultaRelatorio(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
