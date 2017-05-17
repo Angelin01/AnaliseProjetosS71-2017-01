59df984fea33d069aa2413f78f9287695a2b8d7f
rafaeltoyo <rafael_toyo@hotmail.com>
2017-05-17 00:14:46 -0300

    consulta pedido

--- InterfaceWpf/InterfaceWpf/Interface/EditarPedido.xaml.cs
+++ InterfaceWpf/InterfaceWpf/Interface/EditarPedido.xaml.cs
@@ -60,18 +60,7 @@ namespace InterfaceWpf
 
             if (messageBoxResult == MessageBoxResult.Yes)
             {
-                Controller user = Controller.Instance;
-
-                Window main_window;
-                if (user.Login == "admin")
-                {
-                    main_window = new InicioAdministracao();
-                }
-                else
-                {
-                    main_window = new InicioFuncionario();
-                }
-                App.Current.MainWindow = main_window;
+                App.Current.MainWindow = new ConsultaPedido();
                 this.Close();
                 App.Current.MainWindow.Show();
             }
