d66ca5e52991ea683f0219e80c1c6f86f089dcfe
Victor Barpp Gomes <bioniclenuparu@gmail.com>
2017-06-16 16:55:14 -0300

    Coloquei hasher para senhas

--- InterfaceWpf/InterfaceWpf/Interface/RegistraFuncionario.xaml.cs
+++ InterfaceWpf/InterfaceWpf/Interface/RegistraFuncionario.xaml.cs
@@ -169,9 +169,12 @@ namespace InterfaceWpf
 
             MessageBoxResult messageBoxResult = System.Windows.MessageBox.Show("Confirma os dados inseridos?", "Confirmação", System.Windows.MessageBoxButton.YesNo);
 			if (messageBoxResult == MessageBoxResult.Yes) {
-				Funcionario f = new Funcionario(box_nome.Text, box_nome_da_mae.Text, box_nome_do_pai.Text, box_cpf.Text, box_rg.Text, box_ctps.Text, box_endereco.Text, box_telefone.Text, box_telefone_cel.Text, box_email.Text, box_email_alt.Text, box_login.Text, box_senha.Text, Convert.ToInt32(box_salario.Text), box_cargo.Text);
+				var hash_senha = SecurePasswordHasher.Hash(box_senha.Text);
+
+				Funcionario f = new Funcionario(box_nome.Text, box_nome_da_mae.Text, box_nome_do_pai.Text, box_cpf.Text, box_rg.Text, box_ctps.Text, box_endereco.Text, box_telefone.Text, box_telefone_cel.Text, box_email.Text, box_email_alt.Text, box_login.Text, hash_senha, Convert.ToInt32(box_salario.Text), box_cargo.Text);
 
 				if(!f.CadastrarDadosFuncionario()) {
+					MessageBox.Show("Já existe um funcionário cadastrado com este CPF.\nPor favor, retorne à tela de consulta e selecione a operação de Editar ou Remover.", "Erro");
 					return;
 				}
 
