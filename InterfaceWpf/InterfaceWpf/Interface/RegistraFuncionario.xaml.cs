1dba61d77344580a8e695e93b4dbf738797360c8
Angelin01 <angeloborsoiross@hotmail.com>
2017-06-16 21:16:07 -0300

    Movido Validar CPF pra Funcionario.cs - BUGADO

--- InterfaceWpf/InterfaceWpf/Interface/RegistraFuncionario.xaml.cs
+++ InterfaceWpf/InterfaceWpf/Interface/RegistraFuncionario.xaml.cs
@@ -157,62 +157,6 @@ namespace InterfaceWpf
             return true;
         }
 
-        private bool Validar_Cpf(string cpf) {
-
-            int[] mult1 = new int[9] {10, 9, 8, 7, 6, 5, 4, 3, 2};
-            int[] mult2 = new int[10] {11, 10, 9, 8, 7, 6, 5, 4, 3, 2};
-            string tempCpf;
-            string digito;
-            int soma = 0;
-            int resto;
-
-            cpf = cpf.Trim();
-            cpf = cpf.Replace(".", "").Replace("-", ""); // Tirar espaços, "."s e "-"s
-
-            if (cpf.Length != 11) {
-                return false;
-            }
-            foreach (char c in cpf) { 
-                if (c < '0' || c > '9') { // Só pode ter sobrado números
-                    return false;
-                }
-            }
-
-            tempCpf = cpf.Substring(0, 9);
-
-            for (int i = 0; i < 9; i++) {
-                soma += int.Parse(tempCpf[i].ToString()) * mult1[i]; // Fazendo as primeiras multiplicações
-            }
-
-            resto = soma % 11;
-            if (resto < 2) {
-                resto = 0;
-            }
-            else {
-                resto = 11 - resto;
-            }
-
-            digito = resto.ToString();
-
-            tempCpf = tempCpf + digito;
-
-            soma = 0;
-            for (int i = 0; i < 10; i++) {
-                soma += int.Parse(tempCpf[i].ToString()) * mult2[i];
-            }
-
-            resto = soma % 11;
-            if (resto < 2) {
-                resto = 0;
-            }
-            else {
-                resto = 11 - resto;
-            }
-
-            digito = digito + resto.ToString();
-            return cpf.EndsWith(digito);
-        }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Controller user = Controller.Instance;
