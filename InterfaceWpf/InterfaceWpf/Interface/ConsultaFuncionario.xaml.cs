83fa656e8362db8057a7ff2f45060fcff6291123
Victor Barpp Gomes <bioniclenuparu@gmail.com>
2017-06-17 18:25:41 -0300

    Feito filtro de consulta de funcionários

--- InterfaceWpf/InterfaceWpf/Interface/ConsultaFuncionario.xaml.cs
+++ InterfaceWpf/InterfaceWpf/Interface/ConsultaFuncionario.xaml.cs
@@ -80,6 +80,79 @@ namespace InterfaceWpf.Interface
 			}
 		}
 
+		public void FiltrarLista()
+		{
+			using (MySqlConnection conn = new MySqlConnection(Controller.Instance.connStr)) {
+				try {
+					conn.Open();
+				}
+				catch (MySqlException ex) {
+					// Conexão com o banco de dados falhou.
+					// Possíveis razões: Fora do ar, ou usuário/senha incorretos
+					//MessageBox.Show(ex.Message);
+					return;
+				}
+
+				MySqlCommand cmd = new MySqlCommand();
+				cmd.Connection = conn;
+
+				cmd.CommandText = "SELECT nome, nome_da_mae, nome_do_pai, cpf, rg, ctps, endereco, telefone, telefone_cel, email, email_alt, login, senha, salario, cargo FROM Funcionario WHERE 1";
+				cmd.Prepare();
+				if (!String.IsNullOrEmpty(txtFiltroCPF.Text)) {
+					cmd.CommandText += " AND cpf=@cpf";
+					cmd.Parameters.AddWithValue("@cpf", txtFiltroCPF.Text);
+				}
+				if (!String.IsNullOrEmpty(txtFiltroCTPS.Text)) {
+					cmd.CommandText += " AND ctps=@ctps";
+					cmd.Parameters.AddWithValue("@ctps", txtFiltroCTPS.Text);
+				}
+				if (!String.IsNullOrEmpty(txtFiltroLogin.Text)) {
+					cmd.CommandText += " AND login=@login";
+					cmd.Parameters.AddWithValue("@login", txtFiltroLogin.Text);
+				}
+				if (!String.IsNullOrEmpty(txtFiltroNome.Text)) {
+					cmd.CommandText += " AND nome=@nome";
+					cmd.Parameters.AddWithValue("@nome", txtFiltroNome.Text);
+				}
+				if (!String.IsNullOrEmpty(txtFiltroRG.Text)) {
+					cmd.CommandText += " AND rg=@rg";
+					cmd.Parameters.AddWithValue("@rg", txtFiltroRG.Text);
+				}
+
+				MySqlDataReader reader;
+				try {
+					reader = cmd.ExecuteReader();
+				}
+				catch (MySqlException ex) {
+					// Query falhou.
+					return;
+				}
+
+				while (reader.Read()) {
+					Funcionario f = new Funcionario(
+						reader.GetString(0),
+						reader.GetString(1),
+						reader.GetString(2),
+						reader.GetString(3),
+						reader.GetString(4),
+						reader.GetString(5),
+						reader.GetString(6),
+						reader.GetString(7),
+						reader.GetString(8),
+						reader.GetString(9),
+						reader.GetString(10),
+						reader.GetString(11),
+						reader.GetString(12),
+						reader.GetInt32(13),
+						reader.GetString(14)
+						);
+					lvUsers.Items.Add(f);
+				}
+
+				conn.Close();
+			}
+		}
+
 		private void Button_Click(object sender, RoutedEventArgs e)
         {
 
@@ -133,7 +206,9 @@ namespace InterfaceWpf.Interface
 
         private void Button_Filtro(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Resultados filtrados.", "Sucesso!");
+			lvUsers.Items.Clear();
+			FiltrarLista();
+			MessageBox.Show("Resultados filtrados.", "Sucesso!");
         }
     }
 }
